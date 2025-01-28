44ecb68978e044ffa03b2d2231ebd2e25f56a142
mkGerber <michaelgerber2004@gmail.com>
2025-01-27 21:34:08 -0500

    directory fix

--- src/ProjectDetail.jsx
+++ src/ProjectDetail.jsx
@@ -1,22 +1,22 @@
-// ProjectDetail.js
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import './ProjectDetail.css';
 
-import GetOut1 from './images/GetOut.jpg';
-import GetOut2 from './images/GetOut2.PNG';
-import GetOut3 from './images/GetOut3.PNG';
-import Portfolio1 from './images/PortfolioWebsite1.png';
-import Portfolio2 from './images/PortfolioWebsite2.png';
-import Portfolio3 from './images/PortfolioWebsite3.png';
-import AutoWrld1 from './images/Auto-wrld.jpg';
-import AutoWrld2 from './images/Auto-wrld2.png';
-import AUS1 from './images/AUS1.png';
-import AUS2 from './images/AUS2.png';
-import AUS3 from './images/AUS3.jpg';
-import AUS4 from './images/AUS4.jpg';
-import AUS5 from './images/AUS5.jpg';
-import AUSVideo from './images/AUS-V1.MOV';
+// Import images explicitly
+import GetOut1 from '../public/assets/GetOut.jpg';
+import GetOut2 from '../public/assets/GetOut2.png';
+import GetOut3 from '../public/assets/GetOut3.png';
+import Portfolio1 from '../public/assets/PortfolioWebsite1.png';
+import Portfolio2 from '../public/assets/PortfolioWebsite2.png';
+import Portfolio3 from '../public/assets/PortfolioWebsite3.png';
+import AutoWrld1 from '../public/assets/Auto-wrld.jpg';
+import AutoWrld2 from '../public/assets/Auto-wrld2.png';
+import AUS1 from '../public/assets/AUS1.png';
+import AUS2 from '../public/assets/AUS2.png';
+import AUS3 from '../public/assets/AUS3.jpg';
+import AUS4 from '../public/assets/AUS4.jpg';
+import AUS5 from '../public/assets/AUS5.jpg';
+import AUSVideo from '../public/assets/AUS-V1.mov';
 
 function ProjectDetail() {
   const { id } = useParams();
