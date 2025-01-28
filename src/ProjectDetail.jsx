b9b95b9aa33e86ddae78ae147539755b435cbd3a
mkGerber <michaelgerber2004@gmail.com>
2025-01-27 22:07:12 -0500

    fixed file name

--- src/ProjectDetail.jsx
+++ src/ProjectDetail.jsx
@@ -3,25 +3,25 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import './ProjectDetail.css';
 
-import GetOut1 from '..public/assets/GetOut.jpg';
-import GetOut2 from '..public/assets/GetOut2.png';
-import GetOut3 from '..public/assets/GetOut3.png';
-import Portfolio1 from '..public/assets/PortfolioWebsite1.png';
-import Portfolio2 from '..public/assets/PortfolioWebsite2.png';
-import Portfolio3 from '..public/assets/PortfolioWebsite3.png';
-import AutoWrld1 from '..public/assets/Auto-wrld.jpg';
-import AutoWrld2 from '..public/assets/Auto-wrld2.png';
-import AutoWrld3 from '..public/assets/Auto-wrld3.jpg';
-import AutoWrld4 from '..public/assets/Auto-wrld4.png';
-import AUS1 from '..public/assets/AUS1.png';
-import AUS2 from '..public/assets/AUS2.png';
-import AUS3 from '..public/assets/AUS3.jpg';
-import AUS4 from '..public/assets/AUS4.jpg';
-import AUS5 from '..public/assets/AUS5.jpg';
-import AUSVideo from '..public/assets/AUS-V1.mov';
-import GameWishlist1 from '..public/assets/GameWishlist1.png'; 
-import GameWishlist2 from '..public/assets/GameWishlist2.png';
-import GameWishlist3 from '..public/assets/GameWishlist3.png';
+import GetOut1 from '../public/assets/GetOut.jpg';
+import GetOut2 from '../public/assets/GetOut2.png';
+import GetOut3 from '../public/assets/GetOut3.png';
+import Portfolio1 from '../public/assets/PortfolioWebsite1.png';
+import Portfolio2 from '../public/assets/PortfolioWebsite2.png';
+import Portfolio3 from '../public/assets/PortfolioWebsite3.png';
+import AutoWrld1 from '../public/assets/Auto-wrld.jpg';
+import AutoWrld2 from '../public/assets/Auto-wrld2.png';
+import AutoWrld3 from '../public/assets/Auto-wrld3.png';
+import AutoWrld4 from '../public/assets/Auto-wrld4.png';
+import AUS1 from '../public/assets/AUS1.png';
+import AUS2 from '../public/assets/AUS2.png';
+import AUS3 from '../public/assets/AUS3.jpg';
+import AUS4 from '../public/assets/AUS4.jpg';
+import AUS5 from '../public/assets/AUS5.jpg';
+import AUSVideo from '../public/assets/AUS-V1.mov';
+import GameWishlist1 from '../public/assets/GameWishlist1.png'; 
+import GameWishlist2 from '../public/assets/GameWishlist2.png';
+import GameWishlist3 from '../public/assets/GameWishlist3.png';
 
 
 function ProjectDetail() {
