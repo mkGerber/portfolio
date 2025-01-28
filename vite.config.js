47190750a8827ffe4a7bdf625e2e23220b680417
mkGerber <michaelgerber2004@gmail.com>
2025-01-27 22:47:21 -0500

    fixc

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: "/portfolio/",
   assetsInclude: ["**/*.MOV"], // Include `.MOV` files in the build
 }));
