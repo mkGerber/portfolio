1d9e36babae4614212a345bd18d1a942ad707c6e
mkGerber <michaelgerber2004@gmail.com>
2025-01-27 22:29:34 -0500

    fixed?

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useTransition, useDeferredValue } from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from "react";
 import Navbar from "./components/Navbar.jsx";
@@ -7,14 +8,28 @@ import Projects from "./Projects.jsx";
 import ProjectDetail from "./ProjectDetail.jsx";
 
 function App() {
+  const [isPending, startTransition] = useTransition();
+
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/project/:id" element={<ProjectDetail />} />
+        <Route 
+          path="/" 
+          element={<Home />} 
+        />
+        <Route 
+          path="/resume" 
+          element={<Resume />} 
+        />
+        <Route 
+          path="/projects" 
+          element={<Projects />} 
+        />
+        <Route 
+          path="/project/:id" 
+          element={<ProjectDetail />} 
+        />
       </Routes>
     </Router>
   );
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Home.css';
 import Navbar from './components/Navbar.jsx'; // No curly braces for default exports
 import Footer from './components/Footer.jsx';
 import Headshot from './Headshot.jpg';
 
 function Home() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true); // Ensure that content is fully loaded
+  }, []);
+
   return (
     <>
-      <main div className="home">
+      <main className={loaded ? 'fadeIn' : ''}>
         <div className="aboutMe">
           <p>
             JMU student studying computer science. I have skills in front-end development which includes HTML, CSS, JavaScript, React, and styled-components. I also have experience with back-end development using python, SQL, APIs and databases. I am currently seeking an internship for the summer of 2025 to gain more experience in my relevant fields of study.
           </p>
-
         </div>
         <div className="picture">
           <img src={Headshot} alt="Michael Gerber" />
