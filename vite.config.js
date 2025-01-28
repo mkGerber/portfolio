65762e1c26dffbe38551e8f979620cca548d53f1
mkGerber <michaelgerber2004@gmail.com>
2025-01-27 23:06:02 -0500

    index?

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/portfolio/",
+  base: "/portfolio/index",
   assetsInclude: ["**/*.MOV"], // Include `.MOV` files in the build
 }));
