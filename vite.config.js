5f87981e54f80a06eb871df3db9c985d20a8f219
mkGerber <michaelgerber2004@gmail.com>
2025-02-03 16:26:56 -0500

    this should do it

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/portfolio/",
+  base: "/",
   assetsInclude: ["**/*.MOV"], // Include `.MOV` files in the build
 }));
